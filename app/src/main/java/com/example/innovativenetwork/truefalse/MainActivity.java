b37d48e87462873330eeb8ca3746c7be23ed5d99
Shariq <shariqsb93@gmail.com>
2017-05-03 16:25:57 +0500

    Implemented Sugar ORM for Databse

--- app/src/main/java/com/example/innovativenetwork/truefalse/MainActivity.java
+++ app/src/main/java/com/example/innovativenetwork/truefalse/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.innovativenetwork.truefalse;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.orm.SugarContext;
+import com.orm.SugarDb;
+import com.orm.SugarRecord;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
@@ -18,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        SugarContext.init(this);
 
         btn_start = (Button) findViewById(R.id.btn_start);
         btn_option = (Button) findViewById(R.id.btn_options);
@@ -27,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         btn_option.setOnTouchListener(this);
         btn_start.setOnTouchListener(this);
 
+
+
+        showDebugDBAddressLogToast();
 
     }
 
@@ -73,5 +84,18 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 break;
         }
         return true;
+    }
+    public static void showDebugDBAddressLogToast() {
+        if (BuildConfig.DEBUG) {
+            try {
+                Class<?> debugDB = Class.forName("com.amitshekhar.DebugDB");
+                Method getAddressLog = debugDB.getMethod("getAddressLog");
+                Object value = getAddressLog.invoke(null);
+
+                Log.i("=========", "showDebugDBAddressLogToast: " + (String) value);
+            } catch (Exception ignore) {
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/innovativenetwork/truefalse/QuestionData.java
@@ -0,0 +1,69 @@
+package com.example.innovativenetwork.truefalse;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by DeLL on 03-May-17.
+ */
+
+public class QuestionData extends SugarRecord {
+     int dificulty, category;
+     boolean isAnswered, answer, givenAnswer;
+     String imageBase64, questionText;
+    public QuestionData(){}
+    public int getDificulty() {
+        return dificulty;
+    }
+
+    public void setDificulty(int dificulty) {
+        this.dificulty = dificulty;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public boolean isAnswered() {
+        return isAnswered;
+    }
+
+    public void setAnswered(boolean answered) {
+        isAnswered = answered;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.answer = answer;
+    }
+
+    public boolean isGivenAnswer() {
+        return givenAnswer;
+    }
+
+    public void setGivenAnswer(boolean givenAnswer) {
+        this.givenAnswer = givenAnswer;
+    }
+
+    public String getImageBase64() {
+        return imageBase64;
+    }
+
+    public void setImageBase64(String imageBase64) {
+        this.imageBase64 = imageBase64;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+}
